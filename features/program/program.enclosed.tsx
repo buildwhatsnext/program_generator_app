3b4301ddb730c7ccf1d0b96315adf610b229e6a3
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-13 13:14:42 -0400

    experimenting with the table

--- features/program/program.enclosed.tsx
+++ features/program/program.enclosed.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Page } from '../../components/pages/page';
 import { ROUTES } from '../../constants/routes';
+import SpaceData from '../../components/display/display.table';
 
 export function EnclosedWorkspaces() {
   return (
-    <Page nextRoute={ ROUTES.PROGRAM.UPDATE } />
+    <Page nextRoute={ ROUTES.PROGRAM.UPDATE }>
+      <WorkspaceDataEntrySection />
+    </Page>
+  )
+}
+
+export function WorkspaceDataEntrySection() {
+  return (
+    <DataEntrySection title="Enclosed Offices" />
+  )
+}
+
+export function DataEntrySection({title, content = null}) {
+  
+  return (
+    <div className="section">
+      <div className="section__title">{title}</div>
+      {/* <div className="section__content">{content}</div> */}
+      <div className="section__content">
+        <SpaceData />
+      </div>
+    </div>
   )
 }
