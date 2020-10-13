efe970fb89dd71cae6519c44dcb23746a505c19b
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-10-13 16:46:06 -0400

    target page codes are in

--- features/info/general.target.tsx
+++ features/info/general.target.tsx
@@ -15,71 +15,31 @@ function TargetMetric () {
   const Q2 = <p>What's the <em> target planning factor? </em> </p>;
   const Q3 = <p>What's the <em> target area per workseat </em> </p>;
   const Q4 = <p>If you have it, what are the <em> target workseats </em> ?</p>;
-  const next = ROUTES.PROGRAM.START;
+  const next = ROUTES.INFO.CONSTRAINTS;
 
   return (
-
+    
     <Page nextRoute={next}>
       <QnA 
         question={Q1} 
         answers={[<TextInputBox content={`Enter the target circulation factor (%)`}/> ]} 
       />
+
+      <QnA 
+        question={Q2} 
+        answers={[<TextInputBox content={`Enter the target planning factor (%)`}/> ]} 
+      />
+
+      <QnA 
+        question={Q3} 
+        answers={[<TextInputBox content={`Enter the target area per workseat (%)`}/> ]} 
+      />
+
+      <QnA 
+        question={Q4} 
+        answers={[<TextInputBox content={`Enter the total area of the space`}/> ]} 
+      />
     </Page>
-    /*
-    <div className={styles.page}>
-      <div className={styles.page__content}>
-
-        <QnA
-          question={Q1}
-          answers={[
-            <TextInputBox content="Enter the target circulation factor (%)"/>
-          ]}
-        />
-
-        <div>
-          -
-        </div>
-
-        <QnA
-          question={Q2}
-          answers={[
-            <TextInputBox content="Enter the target planning factor (%)"/>
-          ]}
-        />
-
-        <div>
-          -
-        </div>
-
-        <QnA
-          question={Q3}
-          answers={[
-            <TextInputBox content="Enter the target area per workseat (%)"/>
-          ]}
-        />
-
-        <div>
-          -
-        </div>
-
-        <QnA
-          question={Q4}
-          answers={[
-            <TextInputBox content="Enter the total area of the space"/>
-          ]}
-        />
-
-      </div>
-
-      <div className={styles.page__navigation}>
-        <BackButton />
-        <DirectionalButton location={ROUTES.PROGRAM.START} content="Next" />
-      </div>
-
-    </div>
-
-    */
-
   );
 };
 
