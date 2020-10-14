2e0c3ba28b1d67c1402c624726374717a952deb1
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-14 18:14:33 -0400

    added fakePie

--- components/display/display.pie.tsx
+++ components/display/display.pie.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Pie } from 'react-chartjs-2';
 
 export function ProgrammedSpaceDisplay() {
+  const labels = [
+    '18 - 34',
+    '34 - 51',
+    '52+',
+  ]
+
+  const dataSets = [{
+    data: [2000, 4000, 2850],
+    backgroundColor: ['red', 'blue', 'green']
+  }];
+
   return (
-    <h1>Programmed Space</h1>
+    <div className="pie">
+      <Pie
+        data={
+          {labels: labels,
+          datasets: dataSets}
+        }
+      />
+    </div>
   )
 }
