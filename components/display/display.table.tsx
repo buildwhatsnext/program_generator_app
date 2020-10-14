6b4652d8af089bda8c4ec4929c12e8670f36290a
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-14 12:44:00 -0400

    abstracted enclosed spaces data entry

--- components/display/display.table.tsx
+++ components/display/display.table.tsx
@@ -6,7 +6,6 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import TextField from '@material-ui/core/TextField';
 import { ISpace, SpaceType } from '../../components/spaces/space.type';
@@ -56,37 +55,38 @@ function createData(name: string, code: string, population: number, size: number
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+// const rows = [
+//   createData('India', 'IN', 1324171354, 3287263),
+//   createData('China', 'CN', 1403500365, 9596961),
+//   createData('Italy', 'IT', 60483973, 301340),
+//   createData('United States', 'US', 327167434, 9833520),
+//   createData('Canada', 'CA', 37602103, 9984670),
+//   createData('Australia', 'AU', 25475400, 7692024),
+//   createData('Germany', 'DE', 83019200, 357578),
+//   createData('Ireland', 'IE', 4857000, 70273),
+//   createData('Mexico', 'MX', 126577691, 1972550),
+//   createData('Japan', 'JP', 126317000, 377973),
+//   createData('France', 'FR', 67022000, 640679),
+//   createData('United Kingdom', 'GB', 67545757, 242495),
+//   createData('Russia', 'RU', 146793744, 17098246),
+//   createData('Nigeria', 'NG', 200962417, 923768),
+//   createData('Brazil', 'BR', 210147125, 8515767),
+// ];
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     width: '100%',
+//   },
+//   container: {
+//     maxHeight: 440,
+//   },
+// });
 
 export default function StickyHeadTable() {
-  const classes = useStyles();
+  let start = new Array(2);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowData, setRowData] = React.useState(start);
 
   return (
     <Paper /* className={classes.root} */>
@@ -106,25 +106,22 @@ export default function StickyHeadTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                  {columns.map((column) => {
-                    const value = row[column.id];
-                    return (
-                      <TableCell key={column.id} align={column.align}>
-                        {
-                          // column.format && typeof value === 'number' 
-                          //   ? column.format(value) 
-                          //   : value
+            {
+              rowData.map( (r,i) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={i++}>
+                    {columns.map((column) => {
+                      // const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
                           <TextField id="standard-basic" />
-                        }
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })
+            }
           </TableBody>
         </Table>
       </TableContainer>
--- features/program/program.enclosed.tsx
+++ features/program/program.enclosed.tsx
@@ -6,24 +6,23 @@ import SpaceData from '../../components/display/display.table';
 export function EnclosedWorkspaces() {
   return (
     <Page nextRoute={ ROUTES.PROGRAM.UPDATE }>
-      <WorkspaceDataEntrySection />
+      <WorkspaceDataEntrySection title="Enclosed Offices" data={<SpaceData />} />
     </Page>
   )
 }
 
-export function WorkspaceDataEntrySection() {
+export function WorkspaceDataEntrySection({title, data}) {
   return (
-    <DataEntrySection title="Enclosed Offices" />
+    <DataEntrySection title={title} data={data}/>
   )
 }
 
-export function DataEntrySection({title, content = null}) {
-  
+export function DataEntrySection({title, data = null}) {
   return (
     <div className="section">
       <div className="section__title">{title}</div>
       <div className="section__content">
-        <SpaceData />
+        { data }
       </div>
     </div>
   )
