27ad49df4155c9f5d5db029522e37be258aa8d38
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-14 16:43:34 -0400

    added lab spaces

--- constants/routes.ts
+++ constants/routes.ts
@@ -24,6 +24,7 @@ export const ROUTES = {
     SUPPORT_UPDATE: '/program/support_update',
     BROADCAST_UPDATE: '/program/broadcast_update',
     LAB_UPDATE: '/program/lab_update',
+    END: '/program/end',
   },
   TRANSITION: {
     INFO: '/transition/info',
---
+++ features/program/program.lab.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Page } from '../../components/pages/page';
+import { ROUTES } from '../../constants/routes';
+import SpaceData from '../../components/display/display.table';
+import { WorkspaceDataEntrySection } from './program.data';
+
+export function LabSpaces() {
+  return (
+    <Page nextRoute={ ROUTES.PROGRAM.LAB_UPDATE }>
+      <WorkspaceDataEntrySection title="Lab Spaces" data={<SpaceData />} />
+    </Page>
+  )
+}
