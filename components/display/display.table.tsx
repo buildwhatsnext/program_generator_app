a02817ae616aecb5f86bcfabfe82f3c1494d0ed6
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-14 12:53:44 -0400

    added space columns

--- components/display/display.table.tsx
+++ components/display/display.table.tsx
@@ -11,8 +11,10 @@ import TextField from '@material-ui/core/TextField';
 import { ISpace, SpaceType } from '../../components/spaces/space.type';
 
 interface ISpaceColumn {
-  type: SpaceType,
+  id: string,
+  // type: SpaceType,
   label: string,
+  minWidth?: number,
   format?: (value: number) => string
 }
 
@@ -23,69 +25,51 @@ function convertEnumToString(value: SpaceType) {
 }
 
 const columns: ISpaceColumn[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { 
+    id: 'name', 
+    label: 'Name', 
+    minWidth: 170 
+  },
+  { 
+    id: 'seats', 
+    label: 'Seats', 
+    minWidth: 100 
+  },
   {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
+    id: 'ratio',
+    label: 'Ratio',
+    minWidth: 100,
+    // align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
+    id: 'area',
+    label: 'Area',
+    minWidth: 100,
+    // align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
+    id: 'qty_selected',
+    label: 'Quantity Selected',
+    minWidth: 100,
+    // align: 'right',
     format: (value: number) => value.toFixed(2),
+  },
+  { 
+    id: 'seats_total', 
+    label: 'Total Seats', 
+    minWidth: 100 
+  },
+  { 
+    id: 'area_total', 
+    label: 'Total Area', 
+    minWidth: 100 
   },
 ];
 
-
-
-function createData(name: string, code: string, population: number, size: number): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-// const rows = [
-//   createData('India', 'IN', 1324171354, 3287263),
-//   createData('China', 'CN', 1403500365, 9596961),
-//   createData('Italy', 'IT', 60483973, 301340),
-//   createData('United States', 'US', 327167434, 9833520),
-//   createData('Canada', 'CA', 37602103, 9984670),
-//   createData('Australia', 'AU', 25475400, 7692024),
-//   createData('Germany', 'DE', 83019200, 357578),
-//   createData('Ireland', 'IE', 4857000, 70273),
-//   createData('Mexico', 'MX', 126577691, 1972550),
-//   createData('Japan', 'JP', 126317000, 377973),
-//   createData('France', 'FR', 67022000, 640679),
-//   createData('United Kingdom', 'GB', 67545757, 242495),
-//   createData('Russia', 'RU', 146793744, 17098246),
-//   createData('Nigeria', 'NG', 200962417, 923768),
-//   createData('Brazil', 'BR', 210147125, 8515767),
-// ];
-
-// const useStyles = makeStyles({
-//   root: {
-//     width: '100%',
-//   },
-//   container: {
-//     maxHeight: 440,
-//   },
-// });
-
 export default function StickyHeadTable() {
   let start = new Array(2);
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [rowData, setRowData] = React.useState(start);
 
   return (
