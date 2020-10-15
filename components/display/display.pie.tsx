466bbf3263008206e2596350f1f6c97b9de6c61c
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-15 08:44:36 -0400

    got the geneeral gist of the doughnut

--- components/display/display.pie.tsx
+++ components/display/display.pie.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Pie } from 'react-chartjs-2';
+import { Pie, Doughnut } from 'react-chartjs-2';
 import styles from './display.pie.module.scss';
 
 export function ProgrammedSpaceDisplay() {
   const labels = [
     '18 - 34',
     '34 - 51',
-    '52+',
+    '52 - 75',
+    '75+',
   ]
 
   const dataSets = [{
-    data: [2000, 4000, 2850],
-    backgroundColor: ['red', 'blue', 'green']
+    data: [2000, 4000, 2850, 150],
+    backgroundColor: ['red', 'blue', 'green', 'orange']
   }];
+
+  const options = {
+    segmentShowStroke: true,
+    segmentStrokeColor: '#fff',
+    segmentStrokeWidth: 2,
+    percentageInnerCutout: 50,
+  }
 
   return (
     <div className={styles.pie}>
-      <Pie
+      <Doughnut
         data={
           {labels: labels,
           datasets: dataSets}
         }
+        options={options}
         height={100}
       />
     </div>
