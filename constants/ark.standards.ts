c1c8bc15ab48b8a29bebce2abbccc3f92cbaed05
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-15 10:10:49 -0400

    added/modded standard ark colors

--- constants/ark.standards.ts
+++ constants/ark.standards.ts
@@ -1,26 +1,30 @@
 export const SPACE_STANDARDS = {
   AMENITY: {
     label: 'Amenity/Special',
-    color: '#ffffff'
+    color: '#F08A02'
   },
   BROADCAST: {
     label: 'Broadcast',
     color: '#7030A0'
   },
   CIRCULATION: {
-    label: 'Circulation + Planning Factor',
+    label: 'Circulation',
     color: '#c4c4c4'
+  },
+  ENCLOSED_OFFICE: {
+    label: 'Offices',
+    color: '#06038D'
   },
   LAB: {
     label: 'Lab',
     color: '#ff0000'
   },
   MEETING: {
-    label: 'Meeting',
+    label: 'Meeting Spaces',
     color: '#ffcc00'
   },
-  OPEN_OFFICE: {
-    label: 'Open Office',
+  OPEN_OFFICE_AREA: {
+    label: 'Open Office Area',
     color: '#0097cc'
   },
   SUPPORT: {
