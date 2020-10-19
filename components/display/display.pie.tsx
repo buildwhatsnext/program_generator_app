19932c6829ead0815cdf69b1e08128d1bb90f637
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-19 13:55:05 -0400

    moved area info into program slice

--- components/display/display.pie.tsx
+++ components/display/display.pie.tsx
@@ -3,14 +3,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Doughnut } from 'react-chartjs-2';
 import styles from './display.pie.module.scss';
 import { SPACE_STANDARDS } from '../../constants/ark.standards';
-import { selectProject } from '../../features/project/project.slice';
+// import { selectProject } from '../../features/project/project.slice';
+import { selectProgram } from '../../features/program/program.slice';
 
 export function ProgrammedSpaceDisplay() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const data = useSelector(selectProject)
+  const { overview } = useSelector(selectProgram)
   const labels = Object.values(SPACE_STANDARDS).map(space => space.name);
   const colors = Object.values(SPACE_STANDARDS).map(space => space.color);
   const borders = Object.values(SPACE_STANDARDS).map(space => space.border);
+
+  // const { basic } = overview;
 
   const datasets = [{
     data: [2000, 4000, 2850, 150],
