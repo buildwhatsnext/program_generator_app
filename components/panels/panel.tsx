45afdc54b1f8bd91f26ff369d23f63f5b52cd88f
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-20 15:22:05 -0400

    fixed panel sizing

--- components/panels/panel.tsx
+++ components/panels/panel.tsx
@@ -10,7 +10,9 @@ export function Panel({title = 'Program Dashboard', children }: IPanel) {
   return (
     <div className={styles.panel}>
       <div className={styles.panel__title}>{title}</div>
-      <div className={styles.panel__content}>{children}</div>
+      <div className={styles.panel__content}>
+        {children}
+      </div>
     </div>
   )
 }
