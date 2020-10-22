2fbcb61c1ba68d54c3274be8af6d175cf65e50b9
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-22 14:49:53 -0400

    made page scrollable again

--- components/display/display.entry.tsx
+++ components/display/display.entry.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 export function DataEntrySection({title, data = null}) {
   return (
     <div className="section">
-      <div className="section__title">{title}</div>
+      <div className="section__title">
+        <h2>
+          {title}
+        </h2>
+      </div>
       <div className="section__content">
         { data }
       </div>
