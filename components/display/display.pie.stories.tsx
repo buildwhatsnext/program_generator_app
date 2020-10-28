3fccc0150a07127a3a2cdc2113d11ceb1a8e4326
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-10-28 10:07:15 -0400

    got rid of type 6-0 from pie.stories

--- components/display/display.pie.stories.tsx
+++ components/display/display.pie.stories.tsx
@@ -1,4 +1,4 @@
-import { Story } from '@storybook/react/types-6-0';
+import { Story, Meta } from '@storybook/react';
 import React from 'react';
 import { ProgrammedSpaceDisplay, PieProps } from './display.pie';
 
