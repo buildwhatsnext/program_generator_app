a14b8dd03c0f09a14299b7190ab63a58ce1d514b
Preston Smith <smith.preston2@gmail.com>
2020-11-03 12:28:10 -0500

    restores state on target page

--- pages/info/target.tsx
+++ pages/info/target.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectOverview } from '../../features/info/info.slice';
 import TargetMetric from '../../features/info/general.target';
 
 export default function TargetMetricPage() {
-  return <TargetMetric />;
+  const { 
+    targetFactorCirculation, 
+    targetFactorLoss, 
+    targetAreaPerWorkseat, 
+    targetNumOfWorkseats 
+  } = useSelector(selectOverview);
+
+  const state = [ 
+    targetFactorCirculation, 
+    targetFactorLoss, 
+    targetAreaPerWorkseat, 
+    targetNumOfWorkseats 
+  ];
+
+  const hasPrevState = state.some(val => val !== null);
+  
+  return <TargetMetric hasPrevState={hasPrevState} />;
 }
