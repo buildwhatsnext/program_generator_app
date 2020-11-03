d5e4c0badf0ffe378dbe65a6a815a1c47494ac05
Preston Smith <smith.preston2@gmail.com>
2020-11-03 10:50:45 -0500

    removed unnecessary style

--- components/info/input.tsx
+++ components/info/input.tsx
@@ -4,17 +4,6 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import styles from './input.module.scss';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  }),
-);
-
 interface ITextInput {
   content?: string;
   // ref: Ref<HTMLInputElement>;
