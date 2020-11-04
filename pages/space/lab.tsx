a9f4c1b94253f73010fa500265c9af392a3dd485
Preston Smith <smith.preston2@gmail.com>
2020-11-04 06:57:36 -0500

    fixed lab

--- pages/space/lab.tsx
+++ pages/space/lab.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import { LabSpaces } from '../../features/space/space.lab';
+import { useSelector } from 'react-redux';
+import { LabSpace } from '../../components/spaces/Space';
+import { hydrateSpaceState } from '../../features/space/space.functions';
+import { selectProgram, setLabData, setLabTotalArea } from '../../features/space/space.slice';
+import { ROUTES } from '../../constants/routes';
+import { GenericSpacePage } from '../../features/space/space.generic';
 
-export default function LabSpacesPage() {
-  return <LabSpaces />
+export default function LabSpacePage() {
+  const program = useSelector(selectProgram);
+  const hasPrevState = program.LabState.length > 0;
+
+  const hydratedState = hydrateSpaceState<LabSpace>(program.LabState);
+
+  return (
+    <GenericSpacePage 
+      pageTitle='Lab Spaces'
+      nextRoute={ROUTES.SPACE.LAB_UPDATE}
+      type={LabSpace}
+      storeHandler={setLabData}
+      areaHandler={setLabTotalArea}
+      hasPrevState={hasPrevState}
+      prevState={hydratedState}
+    />
+  )
 }
