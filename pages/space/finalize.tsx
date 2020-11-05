8c6d12d256bda55ab3e138d6f6c7ea4bee660574
Preston Smith <smith.preston2@gmail.com>
2020-11-05 16:25:24 -0500

    used Space standard to help final page

--- pages/space/finalize.tsx
+++ pages/space/finalize.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { AmenitySpace } from '../../components/spaces/Space';
+import { AmenitySpace, EnclosedOfficeSpace } from '../../components/spaces/Space';
 import { hydrateSpaceState } from '../../features/space/space.functions';
 import { selectProgram, setAmenityData, setAmenityTotalArea } from '../../features/space/space.slice';
 import { ROUTES } from '../../constants/routes';
 import { GenericSpacePage } from '../../features/space/space.generic';
-import { CollapsibleDataEntrySection } from '../../components/display/display.entry';
 import SpaceTable from '../../components/table/table.component';
+import { SPACE_STANDARDS, SPACE_STATE_NAMES } from '../../constants/ark.standards';
+import { selectOverview } from '../../features/info/info.slice';
+import { SpaceDataSection } from '../../features/space/space.section';
+import { Page } from '../../components/pages/page';
 
 export default function SpaceConfirmationPage() {
-  const program = useSelector(selectProgram);
-  const hasPrevState = program.AmenityState.length > 0;
+  const { hasLab, hasBroadcast } = useSelector(selectOverview);
+  const standards = { ...SPACE_STANDARDS };
+  if(!hasLab)
+    delete standards.LAB;
 
-  const hydratedState = hydrateSpaceState<AmenitySpace>(program.AmenityState);
+  if (!hasBroadcast)
+    delete standards.BROADCAST;
+
+  const types = Object.values(standards).filter(standard => standard.type)
+
+  const sections = types.map((type, index) => {
+    return (
+      <div key={`${index}_${type.name}`} style={{marginBottom: '1rem'}}>
+        <SpaceDataSection 
+          collapsible
+          startHidden={index > 0}
+          title={type.sectionTitle}
+          type={type.type}
+          stateName={type.stateName}
+          storeHandler={type.storeHandler}
+          areaHandler={type.areaHandler}
+        />
+      </div>
+      
+    )
+  })
 
   return (
-    <>
-      <CollapsibleDataEntrySection 
-        title='Space One'
-        data={
-          <div>
-            <h1>Some Data!</h1>
-          </div>
-        }
-      />
-      <CollapsibleDataEntrySection 
-        title='Space Two'
-        data={
-          <div>
-            <h1>Some Other Data!</h1>
-          </div>
-        }
-      />
-      <CollapsibleDataEntrySection 
-        title='Space Three'
-        data={
-          <div>
-            <h1>Even More Data!</h1>
-          </div>
-        }
-      />
-    </>
+    <Page nextRoute={ROUTES.SPACE.WRAP} >
+      { sections }
+    </Page>
   )
 }
