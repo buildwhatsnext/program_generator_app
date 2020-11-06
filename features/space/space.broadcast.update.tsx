fdb16b3b1b71b8a6ce4ddcc15a41a7c0227dc035
Preston Smith <smith.preston2@gmail.com>
2020-11-06 07:28:48 -0500

    routes based on broadcast/lab choices

--- features/space/space.broadcast.update.tsx
+++ features/space/space.broadcast.update.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { ProgramTransition } from './space.transition';
+import { selectOverview } from '../info/info.slice';
 import { ROUTES } from '../../constants/routes';
+import { calculatePageAfterBroadcast } from '../../components/Navigator';
 
 export function BroadcastSpaceUpdate() {
   const desc = `Let's take a look at the lab space requirements`;
   const forward = `Start Lab Spaces`;
+  const { hasLab } = useSelector(selectOverview);
+
+  const nextRoute = calculatePageAfterBroadcast(hasLab);
 
   return (
     <ProgramTransition 
       desc={desc}
       fwdBtnText={forward}
-      to={ROUTES.SPACE.LAB}
+      to={nextRoute}
     />
   )
 }
--- features/space/space.support.update.tsx
+++ features/space/space.support.update.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { ProgramTransition } from './space.transition';
-import { ROUTES } from '../../constants/routes';
+import { selectOverview } from '../info/info.slice';
+import { calculatePageAfterSupport } from '../../components/Navigator';
 
 export function SupportSpaceUpdate() {
   const desc = `Let's take a look at the broadcast space requirements`;
   const forward = `Start Broadcast Spaces`;
+  const { hasBroadcast, hasLab } = useSelector(selectOverview);
+
+  const nextRoute = calculatePageAfterSupport(hasBroadcast, hasLab);
 
   return (
     <ProgramTransition 
       desc={desc}
       fwdBtnText={forward}
-      to={ROUTES.SPACE.BROADCAST}
+      to={nextRoute}
     />
   )
 }
+
