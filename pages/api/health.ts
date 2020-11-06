1eb5a14f721d86aeadce4608ac51119c35a0ce69
Preston Smith <smith.preston2@gmail.com>
2020-11-06 17:43:33 -0500

    created simple healthcheck endpoint

---
+++ pages/api/health.ts
@@ -0,0 +1,41 @@
+export default async function handler(req, res) {
+  const { method } = req
+
+  // await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        // const pets = await Pet.find({}) /* find all the data in our database */
+        const pets = {
+          name: 'Poppy the Pet'
+        }
+        res.status(200).json({ success: true, data: pets })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+module.exports = allowCors(handler)
