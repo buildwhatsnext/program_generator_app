f50e03ae34054a22d7ea1de72381828ebf196e78
Preston Smith <smith.preston2@gmail.com>
2020-11-08 10:30:39 -0500

    removed metadata call from db

--- server/config/config.database.ts
+++ server/config/config.database.ts
@@ -1,4 +1,3 @@
-import "reflect-metadata";
 import { createConnection } from "typeorm";
 import OptionsDebug from './config.debug';
 import OptionsTesting from './config.testing';
@@ -14,23 +13,3 @@ async function connectToDatabase() {
 }
 
 export default connectToDatabase;
-
-
-// createConnection()
-//   .then(async connection => {
-
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: ", user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-
-// }).catch(error => console.log(error));
