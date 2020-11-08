d2aa61464c8cc48e67a3ec47b5332deb9fd0aa8f
Preston Smith <smith.preston2@gmail.com>
2020-11-08 10:57:18 -0500

    fixed directory issue so data saves locally

--- server/config/config.debug.ts
+++ server/config/config.debug.ts
@@ -2,11 +2,14 @@ import { ConnectionOptions } from 'typeorm';
 import path from 'path';
 import Project from '../models/model.project';
 
-const root = path.resolve(__dirname, '../../');
+// const root = path.resolve(__dirname, '../../');
+const root = process.cwd();
+// const entities = `${root}/server/models/*.ts`;
 
 const options: ConnectionOptions = {
   type: "sqlite",
   database: `${root}/data/debug.sqlite`,
+  // entities: [ entities ],
   entities: [ Project ],
   logging: true
 }
