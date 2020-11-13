b39ab3f776fc7272b8830ea20803a8cfe453ec93
Preston Smith <smith.preston2@gmail.com>
2020-11-13 10:57:47 -0500

    added ability to build entities

--- server/config/config.base.ts
+++ server/config/config.base.ts
@@ -1,6 +1,5 @@
 import * as dotenv from 'dotenv';
 import { ConnectionOptions } from 'typeorm';
-import path from 'path';
 import Project from '../models/model.project';
 
 const root = process.cwd();
