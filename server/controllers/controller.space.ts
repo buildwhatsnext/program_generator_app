eaa19df88422d670661000e9fc194e3422bd227d
Preston Smith <smith.preston2@gmail.com>
2020-11-17 16:16:48 -0500

    added methods to space controller

--- server/controllers/controller.space.ts
+++ server/controllers/controller.space.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import SpaceRepo from '../repository/repo.space';
-import BaseController from "./controller.abstract";
+import BaseCtrl from "./controller.abstract";
 
-export default class SpaceCtrl extends BaseController {
+export default class SpaceCtrl extends BaseCtrl {
   static async getAll(req: NextApiRequest, res: NextApiResponse) {
     try {
       const data = await SpaceRepo.getAll();
@@ -11,4 +11,55 @@ export default class SpaceCtrl extends BaseController {
       this.handleError(error, req, res);
     }
   }
+
+  
+  static async deleteAllProjects(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      await SpaceRepo.deleteAll();
+      res.status(200).json({status: `deleted all projects!`})
+    } catch(error) {
+      BaseCtrl.handleError(error, req, res);
+    }
+  }
+
+  static async getProjectById(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { query: { id } } = req;
+      const ID =  Array.isArray(id) ? id[0] : id;
+      const data = await SpaceRepo.getById(ID);
+      res.status(200).json(data)
+    } catch(error) {
+      BaseCtrl.handleError(error, req, res);
+    }
+  }
+
+  static async deleteProject(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { query: { id } } = req;
+      const ID =  Array.isArray(id) ? id[0] : id;
+      await SpaceRepo.deleteById(ID);
+      res.status(200).json({status: `deleted project ${ID}`})
+    } catch(error) {
+      BaseCtrl.handleError(error, req, res);
+    }
+  }
+
+  static async saveProject(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { query: { id }, body } = req;
+      const project = await SpaceRepo.updateData({...body});
+      res.status(200).json(project)
+    } catch(error) {
+      BaseCtrl.handleError(error, req, res);
+    }
+  }
+
+  // static async createNewProject(req: NextApiRequest, res: NextApiResponse) {
+  //   try {
+  //     const data = await SpaceRepo.createNew();
+  //     res.status(200).json(data)
+  //   } catch(error) {
+  //     BaseCtrl.handleError(error, req, res);
+  //   }
+  // }
 }
