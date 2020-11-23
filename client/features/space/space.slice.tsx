da2101df27b35b99cdb029608493385caae98657
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-11-23 08:48:36 -0500

    space slice documentation filled in

--- client/features/space/space.slice.tsx
+++ client/features/space/space.slice.tsx
@@ -13,75 +13,171 @@ const programSlice = createSlice({
   initialState: program,
   reducers: {
     /**
-     * @param state: the current state of the application
-     * @param action: yadda yadda
-     * @summary what does this do?
+     * @param state: number
+     * @param action: turning a data that we get from payload into a numerical data
+     * @summary turning a data that we get from payload into a numerical data
      */
     setTotalBuildingArea: (state, action) => {
       console.log('Setting building area')
       const input = tryConvertToNumber(action.payload);
       state.totalAreaContainer = input;
     },
+
+    /**
+     * @param state: number
+     * @param action: turning a data that we get from payload into a numerical data
+     * @summary turning a data that we get from payload into a numerical data
+     */
     setUnprogrammedArea: (state, action) => {
       const input = tryConvertToNumber(action.payload);
       state.totalAreaUnprogrammed = input;
     },
+
+    /**
+     * @param state: number
+     * @param action: turning a data that we get from payload into a numerical data
+     * @summary turning a data that we get from payload into a numerical data
+     */
     setHoldArea: (state, action) => {
       const input = tryConvertToNumber(action.payload);
       state.totalAreaHold = input;
     },
+
+    /**
+     * @param state: string
+     * @param action: 
+     * @summary what does this do?
+     */
     setAmenityData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.AmenityState = input;
     },
+
+    /**
+     * @param state: number
+     * @param action: 
+     * @summary turning the data from the payload into a input which becomes a state for this function
+     */
     setAmenityTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaAmenity = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setBroadcastData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.BroadcastState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setBroadcastTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaBroadcast = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setEnclosedData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.EnclosedState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setEnclosedTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaEnclosed = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setLabData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.LabState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setLabTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaLab = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setMeetingData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.MeetingState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setMeetingTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaMeeting = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setOpenOfficeData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.OpenPlanState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setOpenOfficeTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaOpen = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setSupportData: (state, action: PayloadAction<string[]>) => {
       const input = action.payload;
       state.SupportState = input;
     },
+
+    /**
+     * @param state: the current state of the application
+     * @param action: yadda yadda
+     * @summary what does this do?
+     */
     setSupportTotalArea: (state, action) => {
       const input = action.payload;
       state.totalAreaSupport = input;
