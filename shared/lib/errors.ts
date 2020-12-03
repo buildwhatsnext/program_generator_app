12777969aa2ec78d8ad83ec7e3c2e102fc9235d3
Preston Smith <smith.preston2@gmail.com>
2020-12-03 10:13:34 -0500

    added package to check if we're in a browser/node

--- shared/lib/errors.ts
+++ shared/lib/errors.ts
@@ -1,7 +1,11 @@
+import { isNode } from 'browser-or-node';
+
 export class UnacceptableInputError extends Error {
   constructor(...args) {
     super(...args);
-    Error.captureStackTrace(this, UnacceptableInputError);
+    if(isNode)
+      Error.captureStackTrace(this, UnacceptableInputError);
+      
     const baseMessage = 'The data given was not acceptable for this kind of input';
 
     if(this.message === '' || this.message === null)
