dccb350ff65faee658b03c23e2c3f14bdf4b1540
Preston Smith <smith.preston2@gmail.com>
2020-12-03 16:35:58 -0500

    formats area data in panel

--- client/components/panelsections/section.basic.tsx
+++ client/components/panelsections/section.basic.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { tryConvertToNumber } from '../../../shared/lib/conversion';
+import { formatNumberInput, tryConvertToNumber } from '../../../shared/lib/conversion';
 import { convertDataToINamedValues } from '../text/NamedValue';
 import { formatAreaData } from '../units/units';
 import { INamedPanelSection, PanelSection, reMapPanelData } from './panel.section';
 
 export function BasicInfoPanelSection({ handleClick, isActive, rawData}: INamedPanelSection ) {
   const data = {
-    "Gross Area": 0,
-    "Net Area": 0,
-    "Floors": 0,
+    "Gross Area": '0',
+    "Net Area": '0',
+    "Floors": '0',
     "Circulation Factor": 0,
     "Loss Factor": 0,
   }
 
-  data['Gross Area'] = tryConvertToNumber(rawData.areaGross);
-  data['Net Area'] = tryConvertToNumber(rawData.areaNet);
+  data['Gross Area'] = formatNumberInput(rawData.areaGross);
+  data['Net Area'] = formatNumberInput(rawData.areaNet);
   // eslint-disable-next-line dot-notation
-  data['Floors'] = tryConvertToNumber(rawData.floors);
+  data['Floors'] = formatNumberInput(rawData.floors);
   data['Circulation Factor'] = tryConvertToNumber(rawData.targetFactorCirculation);
   data['Loss Factor'] = tryConvertToNumber(rawData.targetFactorLoss);
 
