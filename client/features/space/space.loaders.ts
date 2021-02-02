0eaaa023854651c5fd47cf51d602dc614971e8cd
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2021-02-02 13:20:19 -0500

    amenity and support spaces have been fully updated with entire space list

--- client/features/space/space.loaders.ts
+++ client/features/space/space.loaders.ts
@@ -13,10 +13,21 @@ export function loadAmenitySpaces() {
   const cafeSeating = new AmenitySpace({ name: `Cafeteria / Seating`, area: 0, seats: 0 });
   const cafeServery = new AmenitySpace({ name: `Cafeteria / Servery`, area: 0, seats: 0 });
   const cafeBoh = new AmenitySpace({ name: `Cafeteria / Back-of-House`, area: 0, seats: 0 });
-  const kitchen = new AmenitySpace({ name: `Kitchen`, area: 0, seats: 0 });
   const pantry = new AmenitySpace({ name: 'Pantry / Teapoint', area: 120, seats: 0 });
+  const kitchen = new AmenitySpace({ name: `Kitchen`, area: 0, seats: 0 });
+  const lounge = new AmenitySpace({ name: `Lounge`, area: 0, seats: 0 });
+  const fitEquip = new AmenitySpace({ name: `Fitness / Equipment`, area: 0, seats: 0 });
+  const fitStudio = new AmenitySpace({ name: `Fitness / Studio`, area: 0, seats: 0 });
+  const fitLock = new AmenitySpace({ name: `Fitness / Locker Rooms`, area: 0, seats: 0 });
+  const vcroom = new AmenitySpace({ name: `Telepresence or Special VC Room`, area: 0, seats: 0 });
+  const training = new AmenitySpace({ name: `Training Room`, area: 0, seats: 0 });
   const reception = new AmenitySpace({ name: 'Reception', area: 360, seats: 0 });
-  const wellness = new AmenitySpace({ name: `Wellness or Mother's Room`, area: 120, seats: 0 });
+  const assembly = new AmenitySpace({ name: `Assembly Space`, area: 0, seats: 0 });
+  const wellness = new AmenitySpace({ name: `Wellness or Mother's Room`, area: 0, seats: 0 });
+  const multifunction = new AmenitySpace({ name: `Wellness or Mother's Room`, area: 0, seats: 0 });
+  const library = new AmenitySpace({ name: `Library`, area: 0, seats: 0 });
+  const nap = new AmenitySpace({ name: `Nap Room`, area: 0, seats: 0 });
+  const showroom = new AmenitySpace({ name: `Demo Room or Showroom`, area: 0, seats: 0 });
 
   return [
     cafeSeating,
@@ -57,12 +68,41 @@ export function loadOpenPlanSpaces() {
 }
 
 export function loadSupportSpaces() {
+  const coat = new SupportSpace({ name: 'Coat Closet', area: 0, seats: 0 });
   const gen = new SupportSpace({ name: 'General Storage', area: 120, seats: 0 });
+  const kit = new SupportSpace({ name: 'Kitchen Storage', area: 0, seats: 0 });
+  const food = new SupportSpace({ name: 'Food Storage', area: 0, seats: 0 });
+  const lock = new SupportSpace({ name: 'Lockers (Non-Fitness)', area: 0, seats: 0 });
+  const mail = new SupportSpace({ name: 'Mail Room', area: 0, seats: 0 });
+  const ship = new SupportSpace({ name: 'Shipping Room', area: 0, seats: 0 });
   const copy = new SupportSpace({ name: 'Copy Room or Copy Area', area: 80, seats: 0 });
+  const filex = new SupportSpace({ name: 'File Area (Enclosed)', area: 0, seats: 0 });
+  const fileo = new SupportSpace({ name: 'File Area (Open)', area: 0, seats: 0 });
+  const tech = new SupportSpace({ name: 'Tech Stop / Help Desk', area: 0, seats: 0 });
+  const ware = new SupportSpace({ name: 'Warehouse', area: 0, seats: 0 });
   const mdf = new SupportSpace({ name: 'MDF', area: 240, seats: 0 });
-  const idf = new SupportSpace({ name: 'IDF', area: 80, seats: 0 });
+  const idf = new SupportSpace({ name: 'IDF', area: 80, seats: 0 });.
+  const wc = new SupportSpace({ name: 'Restrooms (Non-Core)', area: 0, seats: 0 });
+  const mep = new SupportSpace({ name: 'MEP (Non-Core)', area: 0, seats: 0 });
   
-  return [ gen, copy, mdf, idf ];
+  return [
+    coat, 
+    gen,
+    kit,
+    food,
+    lock,
+    mail,
+    ship, 
+    copy,
+    filex,
+    fileo,
+    tech,
+    ware, 
+    mdf, 
+    idf, 
+    wc,
+    mep,
+  ];
 }
 
 export function loadMeetingSpaces() {
