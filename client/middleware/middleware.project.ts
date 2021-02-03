1c29801cbdb999db91a894dfdc9bbdfe7d866f81
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2021-02-03 13:35:18 -0500

    added label bit for middleware for storing the data

--- client/middleware/middleware.project.ts
+++ client/middleware/middleware.project.ts
@@ -6,6 +6,7 @@ import {
   loadProject, 
   setBroadcast, 
   setClient, 
+  setLabel,
   setId, 
   setLab, 
   setTenancy, 
@@ -33,6 +34,7 @@ function setProjectData(data: Partial<ProjectModel>, api: MiddlewareAPI<Dispatch
 
   api.dispatch(setId(data.id))
   api.dispatch(setClient(data.client));
+  api.dispatch(setLabel(data.label));
   api.dispatch(setLab(data.hasLab));
   api.dispatch(setBroadcast(data.hasBroadcast));
   api.dispatch(setTenancy(data.tenancy));
